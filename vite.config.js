49830fd418e1fd085de6d4ef7eb33dbe312deb03
ks4497h-dot <ks4497h@gmail.com>
2025-11-29 19:21:26 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,1 @@
-import { defineConfig } from 'vite';
-export default defineConfig({
-  base: './',
-  define: {
-    'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(process.env.VITE_GEMINI_API_KEY)
-  }
-});
+
